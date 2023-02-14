d094a697e87bfa1f25de4187531befd11f308c27
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-13 20:52:05 -0500

    Condicional Simple

---
+++ main.js
@@ -0,0 +1,8 @@
+//Condicional simple: solo evalua si una condicion es verdadera
+// si no se cumple esa condicion no hace nada
+
+let peso = number(prompt("Cual es su peso?"));
+
+if (peso> 70) {
+   console.log(`Con el peso de ${peso}, creo que estas gordito`)
+}
