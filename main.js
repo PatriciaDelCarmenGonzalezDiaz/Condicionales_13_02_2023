f575e3ff9fba4022184f2a0be918efb2b2549d1e
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-13 21:00:53 -0500

    Condicional Multiple

--- main.js
+++ main.js
@@ -3,8 +3,12 @@
 
 let peso = Number(prompt("Cual es su peso?"));
 
-if (peso> 70) {
-   console.log(`Con el peso de ${peso}, creo que estas gordito`);
+if (peso<=40) {
+   console.log(`Con el peso de ${peso}, creo que estas a punto de morir, COME ALGO`);
+} else if (peso > 40 && peso <= 70){
+    console.log(`Con el peso de ${peso}, creo que estas en buena condicion`);
+} else if (peso > 70 && peso < 90) {
+    console.log(`Con el peso de ${peso}, creo que estas con un poco de sobrepeso`);
 } else {
-    console.log(`Con el peso de ${peso}, creo que estas flaquito`)
+    console.log(`Con el peso de ${peso}, creo que estas con bastante sobrepeso`);
 }
