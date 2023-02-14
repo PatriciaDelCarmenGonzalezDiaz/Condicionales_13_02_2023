d36bcd8563bafec24a52009161d8c2bd0972edae
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-13 21:14:53 -0500

    Condicional_Switch

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-//Condicional if_ else: solo evalua si una condicion es verdadera se realiza una accion
-// si no se cumple esa condicion se realiza otra diferente
+//Condicional Switch: cuando tenemos varias opciones definidas
+//y dependiendo del valor de esa opcion se realizara una operacion
+
 let num1 = Number(prompt("Escribe el primer número"));
 let num2 = Number(prompt("Escribe el segundo número"));
 let operacion = Number(prompt("Operacion a realizar, 1->Suma 2->Resta 3->Multiplicacion 4->Division"));
