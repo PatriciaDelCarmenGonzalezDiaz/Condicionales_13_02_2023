9883cf538d60c2b4deca9e075cc236b5634cf3ea
PatriciaDelCarmenGonzalezDiaz <patgonzale@yahoo.com>
2023-02-13 20:55:40 -0500

    Condicional_If_Else

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-//Condicional simple: solo evalua si una condicion es verdadera
-// si no se cumple esa condicion no hace nada
+//Condicional if_ else: solo evalua si una condicion es verdadera se realiza una accion
+// si no se cumple esa condicion se realiza otra diferente
 
-let peso = number(prompt("Cual es su peso?"));
+let peso = Number(prompt("Cual es su peso?"));
 
 if (peso> 70) {
-   console.log(`Con el peso de ${peso}, creo que estas gordito`)
+   console.log(`Con el peso de ${peso}, creo que estas gordito`);
+} else {
+    console.log(`Con el peso de ${peso}, creo que estas flaquito`)
 }
